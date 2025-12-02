dc5b0b6751a67d32c25329e3623d1fa927c740d3
asterisk727 <59166650+asterisk727@users.noreply.github.com>
2025-12-02 11:08:02 -0500

    chore: initial commit

---
+++ eslint.config.ts
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+        plugins: {
+            js,
+            '@typescript-eslint': tseslint,
+            prettier: prettierPlugin,
+        },
+        extends: ['js/recommended'],
+        languageOptions: { globals: globals.node },
+    },
+    tseslint.configs.recommended,
+    eslintConfigPrettier,
+]);
